188847a1fe784da0bc278d853508bb0463c13411
gabrielelirangi <gabrieleelirangi@gmail.com>
2024-07-11 19:29:48 +0200

    piccoli aggiustamenti

--- src/components/componenti_navbar/BurgherMenu.jsx
+++ src/components/componenti_navbar/BurgherMenu.jsx
@@ -7,7 +7,7 @@ import { X } from "lucide-react";
 function BurgherMenu(){ // passo la prop onclick cosi da far visualizzare al click dell'icona del burghermenu un div con degli elemeni all'interno
     const [mostraMenu, setMostraMenu] = useState(false);
     const [cambiaColoreBurgher, setCambiaColoreBurgher] = useState("white");
-    const [cambiaColoreIconaX, setCambiaColoreIconaX] = useState("#808080");
+    
 
 
     const visualizzaMenu = () => {
@@ -30,14 +30,7 @@ function BurgherMenu(){ // passo la prop onclick cosi da far visualizzare al cli
         setCambiaColoreBurgher("white");
       };
 
-      // funzioni per cambiare colore all'icona x
-      const cambiaColoreX = () => {
-        setCambiaColoreIconaX("red");
-      };
-
-      const ripristinaColoreX = () => {
-        setCambiaColoreIconaX("#808080");
-      };
+      
     
       return (
         <>
@@ -46,8 +39,8 @@ function BurgherMenu(){ // passo la prop onclick cosi da far visualizzare al cli
           {mostraMenu && (
             <div className="menuBurgher">
               <div className="chiudiBurgherMenu">
-                <X color={cambiaColoreIconaX } className="x" size={30} onClick={chiudiMenu} 
-                onMouseEnter={cambiaColoreX} onMouseLeave={ripristinaColoreX} />
+                <X color="#dcdcdc" className="x" size={30} onClick={chiudiMenu} 
+                 />
               </div>
               <div className="menuBurgherlink">
                 <Link to="/" className="bottoniBurgher">Home</Link>
